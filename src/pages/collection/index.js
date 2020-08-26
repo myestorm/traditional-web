df78edc0f9f0cfecc781caa62d3603ed6d46b6e4
wenxue.cwx <coolman_84@163.com>
2020-08-26 22:15:37 +0800

    文集模板

--- src/pages/collection/index.js
+++ src/pages/collection/index.js
@@ -3,3 +3,11 @@ import '@assets/plugins/prism/prism.js'
 import '@assets/scss/vendor.scss'
 import '@assets/scss/markdown.scss'
 import './index.scss'
+
+;(function (window, document, undefined) {
+  const $ = (selector) => {
+    return document.querySelector(selector)
+  }
+  const availHeight = window.screen.availHeight
+  $('.totonoo--collection-article').style.height = `${(availHeight - 60 - 72)}px`
+})(window, document)
